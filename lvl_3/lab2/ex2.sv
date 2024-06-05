c2a0d1c316e4f0c04e8942028ba58c01cf2fd600
Moldir <73649370+Molidier@users.noreply.github.com>
2024-06-05 20:48:06 +0500

    finished

--- lvl_3/lab2/ex2.sv
+++ lvl_3/lab2/ex2.sv
@@ -1,5 +1,17 @@
-/*
-1111
-0111
-0011
-*/
+module top_module (
+    input clk,
+    input shift_ena,
+    input count_ena,
+    input data,
+    output [3:0] q);
+    
+    always @(posedge clk) begin
+        if(shift_ena) begin
+            q <= {q[2:0], data};
+        end
+        else if(count_ena) begin
+            q<= q -1;
+        end
+    end
+
+endmodule
